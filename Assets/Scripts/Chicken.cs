efde8f2a298761e95bcc095dca7c9f65ddb1a64e
Dane Sherman <danemsherman@gmail.com>
2020-02-21 20:50:52 -0500

    Rooster and Chicks

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -13,7 +13,7 @@ public class Chicken : Character
     public ChickenState State
     {
         get { return state; }
-        private set
+        protected set
         {
             state = value;
 
@@ -39,10 +39,10 @@ public class Chicken : Character
         }
     }
 
-    private const float CLOSE_DIST = 1.2f;
-    private const float FLEE_DIST = 3;
-    private const float SAFE_DIST = 5; //should be greater than FLEE_DIST
-
+    protected const float CLOSE_DIST = 1.5f;
+    protected const float FLEE_DIST = 3;
+    protected const float SAFE_DIST = 5; //should be greater than FLEE_DIST
+    
     private void Start()
     {
         State = ChickenState.FLOCK;
@@ -61,7 +61,7 @@ public class Chicken : Character
             if (closestChicken)
             {
                 diffChicken = transform.position - closestChicken.transform.position;
-                if(diffChicken.sqrMagnitude > CLOSE_DIST * CLOSE_DIST)
+                if(diffChicken.sqrMagnitude > SAFE_DIST * SAFE_DIST)
                 {
                     State = ChickenState.FLOCK;
                 }
@@ -77,7 +77,11 @@ public class Chicken : Character
         State = ChickenState.FLEE;
     }
 
-    private IEnumerator Flock()
+    /// <summary>
+    /// Flock while the dog and player are far away and not near a chicken
+    /// </summary>
+    /// <returns></returns>
+    protected virtual IEnumerator Flock()
     {
         Vector2 diffPlayer;
         Vector2 diffDog;
@@ -86,28 +90,34 @@ public class Chicken : Character
             yield return new WaitForEndOfFrame();
 
             movement.Input = Vector2.zero;
-            animator.SetFloat("Speed", movement.Speed);
 
+            //loop over all the other chicken
             Vector2 diffChicken;
             foreach(Chicken chicken in Characters.chicken)
             {
                 if (chicken.Equals(this)) continue; //skip self 
 
+                //check if the chicken is close
                 diffChicken = transform.position - chicken.transform.position;
                 if(diffChicken.sqrMagnitude < CLOSE_DIST * CLOSE_DIST)
                 {
+                    //Enter IDLE state 
                     movement.Input = Vector2.zero;
                     animator.SetFloat("Speed", 0);
 
                     State = ChickenState.IDLE;
                     break;
                 }
+                //check if the chicken is within flee distance
                 if (diffChicken.sqrMagnitude < FLEE_DIST * FLEE_DIST)
                 {
-                    continue;
+                    continue; //skip close-ish
                 }
+                //chicken is far away
                 movement.Seek(chicken.movement);
             }
+
+            animator.SetFloat("Speed", movement.Speed);
 
             diffPlayer = transform.position - Characters.player.transform.position;
             diffDog = transform.position - Characters.dog.transform.position;
@@ -115,12 +125,17 @@ public class Chicken : Character
         } while (diffPlayer.sqrMagnitude > FLEE_DIST * FLEE_DIST &&
                  diffDog.sqrMagnitude > FLEE_DIST * FLEE_DIST);
 
+        //Enter the FLEE state
         movement.Input = Vector2.zero;
         animator.SetFloat("Speed", 0);
 
         State = ChickenState.FLEE;
     }
 
+    /// <summary>
+    /// Run away from the player
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Flee()
     {
         Vector2 diffPlayer;
@@ -129,9 +144,12 @@ public class Chicken : Character
         {
             yield return new WaitForEndOfFrame();
 
+            //flee from both the player and dog
             movement.Input = Vector2.zero;
+
             movement.Flee(Characters.player.movement);
             movement.Flee(Characters.player.movement);
+
             animator.SetFloat("Speed", movement.Speed);
 
             diffPlayer = transform.position - Characters.player.transform.position;
@@ -140,6 +158,7 @@ public class Chicken : Character
         } while (diffPlayer.sqrMagnitude < SAFE_DIST * SAFE_DIST ||
                  diffDog.sqrMagnitude < SAFE_DIST * SAFE_DIST);
 
+        //Enter the FLOCK State
         movement.Input = Vector2.zero;
         animator.SetFloat("Speed", 0);
 
---
+++ Assets/Scripts/Rooster.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooster : Chicken
+{
+    protected override IEnumerator Flock()
+    {
+        Vector2 diffPlayer;
+        Vector2 diffDog;
+        do
+        {
+            yield return new WaitForEndOfFrame();
+
+            movement.Input = Vector2.zero;
+
+            //loop over all the other chicken
+            Vector2 diffChicken;
+            foreach (Chicken chicken in Characters.chicken)
+            {
+                if (chicken.Equals(this)) continue; //skip self 
+
+                //check if the chicken is close
+                diffChicken = transform.position - chicken.transform.position;
+                //check if the chicken is within flee distance
+                if (diffChicken.sqrMagnitude < FLEE_DIST * FLEE_DIST)
+                {
+                    movement.Flee(chicken.movement);
+                }
+                //chicken is far away
+                movement.Seek(chicken.movement);
+            }
+
+            animator.SetFloat("Speed", movement.Speed);
+
+            diffPlayer = transform.position - Characters.player.transform.position;
+            diffDog = transform.position - Characters.dog.transform.position;
+
+        } while (diffPlayer.sqrMagnitude > FLEE_DIST * FLEE_DIST &&
+                 diffDog.sqrMagnitude > FLEE_DIST * FLEE_DIST);
+
+        //Enter the FLEE state
+        movement.Input = Vector2.zero;
+        animator.SetFloat("Speed", 0);
+
+        State = ChickenState.FLEE;
+
+    }
+}
